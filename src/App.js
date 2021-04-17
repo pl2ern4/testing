257c3c84e1a262d868859434c1f3b727ed33ef6c
pl2ern4 <chaturvediprerna03@gmail.com>
2021-04-18 01:17:15 +0530

    modified changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [state, setState] = useState();
+  useEffect(() => {
+    fetch('http://localhost:4001/data')
+      .then(res => res.json())
+      .then(res => {
+        setState(res);
+      })
+  }, []);
+  if (!state) {
+    return null;
+  }
+
+  const getValue = params => params?.content?.text;
+
+  const handleClick = params => () => window.open(params);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        state.offers.map((obj, key) => (
+          <div className="row" key={`data-${key}`}>
+            <div className="rank">{obj.rank}.</div>
+            <span className="teriff-name">{getValue(obj.product)}</span>
+            <div className="streaming">
+              <div className="option download">
+                <div className="text">Download</div>
+                <div className="content">
+                  <div>&darr;</div>
+                  <span>{getValue(obj.contractTerm.downloadSpeed)}</span>
+                </div>
+              </div>
+              <div className="option upload">
+                <div className="text">Upload</div>
+                <div className="content">
+                  <div>&uarr;</div>
+                  <span>{getValue(obj.contractTerm.uploadSpeed)}</span>
+                </div>
+              </div>
+            </div>
+            <ul className="teriff">
+              {obj.remarks.map((o, k) => <li key={`teriff-${key}-${k}`}>{o.content.tooltip.header}</li>)}
+            </ul>
+            <div className="rate">
+              <div>
+                {getValue(obj.campaign.campaignSavings)}
+              </div>
+              <div>
+                <button onClick={handleClick(obj.provider.logoUrl)}>
+                  <span>
+                  {`To ${getValue(obj.provider)}`}
+                  </span>
+                  {'>'}
+                </button>
+              </div>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 }
